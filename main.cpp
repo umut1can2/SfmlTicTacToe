5b1cc0ce859c3d12c849a2be3307a1442ea1c19e
umut1can2 <akyolumut01@hotmail.com>
2016-11-29 08:33:55 +0200

    v0.2

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 using namespace sf;
 using namespace std;
-// Vars //
+//  -*- Vars -*-  //
 
 /// strings ///
 
@@ -30,8 +30,30 @@ bool bool7 = true;
 bool bool8 = true;
 bool bool9 = true;
 
-bool fin = false;
+bool realWin = false;
+void update(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,Text &six,Text &sev,Text &eig,Text &nine){
+	app.draw(one);
+    app.draw(two);
+    app.draw(thr);
+    app.draw(four);
+	app.draw(five);
+	app.draw(six);
+	app.draw(sev);
+	app.draw(eig);
+	app.draw(nine);
+}
 
+void resetColor(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,Text &six,Text &sev,Text &eig,Text &nine){
+	one.setColor( Color::Black );
+	two.setColor( Color::Black );
+	thr.setColor( Color::Black );
+	four.setColor( Color::Black );
+	five.setColor( Color::Black );
+	six.setColor( Color::Black );
+	sev.setColor( Color::Black );
+	eig.setColor( Color::Black );
+	nine.setColor( Color::Black );
+}
 void win(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,Text &six,Text &sev,Text &eig,Text &nine){
     /* FOR X */
     //X's Side
@@ -42,9 +64,15 @@ void win(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,T
       two.setColor( Color::Green );
       thr.setColor( Color::Green );
 
-      app.draw(one);
-      app.draw(two);
-      app.draw(thr);
+      four.setColor( Color::Red );
+      five.setColor( Color::Red );
+      six.setColor( Color::Red );
+      sev.setColor( Color::Red );
+      eig.setColor( Color::Red );
+      nine.setColor( Color::Red );
+
+      update(app,one,two,thr,four,five,six,sev,eig,nine);
+      realWin = true;
     }
     if(c4 == 'X' && c5 == 'X' && c6 == 'X'){
     	cout << "Win X\n";
@@ -53,9 +81,15 @@ void win(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,T
       five.setColor( Color::Green );
       six.setColor( Color::Green );
 
-      app.draw(four);
-      app.draw(five);
-      app.draw(six);
+      one.setColor( Color::Red );
+      two.setColor( Color::Red );
+      thr.setColor( Color::Red );
+      sev.setColor( Color::Red );
+      eig.setColor( Color::Red );
+      nine.setColor( Color::Red );
+
+      update(app,one,two,thr,four,five,six,sev,eig,nine);
+      realWin = true;
     }
     if(c7 == 'X' && c8 == 'X' && c9 == 'X'){
     	cout << "Win X\n";
@@ -64,9 +98,15 @@ void win(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,T
       eig.setColor( Color::Green );
       nine.setColor( Color::Green );
 
-      app.draw(sev);
-      app.draw(eig);
-      app.draw(nine);
+      one.setColor( Color::Red );
+      two.setColor( Color::Red );
+      thr.setColor( Color::Red );
+      four.setColor( Color::Red );
+      five.setColor( Color::Red );
+      six.setColor( Color::Red );
+
+      update(app,one,two,thr,four,five,six,sev,eig,nine);
+      realWin = true;
     }
     //X's Down
     if(c1 == 'X' && c4 == 'X' && c7 == 'X'){
@@ -76,9 +116,15 @@ void win(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,T
       four.setColor( Color::Green );
       sev.setColor( Color::Green );
 
-      app.draw(one);
-      app.draw(four);
-      app.draw(sev);
+      two.setColor( Color::Red );
+      thr.setColor( Color::Red );
+      five.setColor( Color::Red );
+      six.setColor( Color::Red );
+      eig.setColor( Color::Red );
+      nine.setColor( Color::Red );
+
+      update(app,one,two,thr,four,five,six,sev,eig,nine);
+      realWin = true;
     }
     if(c2 == 'X' && c5 == 'X' && c8 == 'X'){
     	cout << "Win X\n";
@@ -87,9 +133,15 @@ void win(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,T
       five.setColor( Color::Green );
       eig.setColor( Color::Green );
 
-      app.draw(two);
-      app.draw(five);
-      app.draw(eig);
+      one.setColor( Color::Red );
+      thr.setColor( Color::Red );
+      four.setColor( Color::Red );
+      six.setColor( Color::Red );
+      sev.setColor( Color::Red );
+      nine.setColor( Color::Red );
+
+      update(app,one,two,thr,four,five,six,sev,eig,nine);
+      realWin = true;
     }
     if(c3 == 'X' && c6 == 'X' && c9 == 'X'){
     	cout << "Win X\n";
@@ -98,9 +150,15 @@ void win(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,T
       six.setColor( Color::Green );
       nine.setColor( Color::Green );
 
-      app.draw(thr);
-      app.draw(six);
-      app.draw(nine);
+      one.setColor( Color::Red );
+      two.setColor( Color::Red );
+      four.setColor( Color::Red );
+      five.setColor( Color::Red );
+      sev.setColor( Color::Red );
+      eig.setColor( Color::Red );
+
+      update(app,one,two,thr,four,five,six,sev,eig,nine);
+      realWin = true;
     }
     //X's Cross
     if(c1 == 'X' && c5 == 'X' && c9 == 'X'){
@@ -110,9 +168,15 @@ void win(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,T
       five.setColor( Color::Green );
       nine.setColor( Color::Green );
 
-      app.draw(one);
-      app.draw(five);
-      app.draw(nine);
+      two.setColor( Color::Red );
+      thr.setColor( Color::Red );
+      four.setColor( Color::Red );
+      six.setColor( Color::Red );
+      sev.setColor( Color::Red );
+      eig.setColor( Color::Red );
+
+      update(app,one,two,thr,four,five,six,sev,eig,nine);
+      realWin = true;
     }if(c3 == 'X' && c5 == 'X' && c7 == 'X'){
     	cout << "Win X\n";
 
@@ -120,9 +184,15 @@ void win(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,T
       five.setColor( Color::Green );
       sev.setColor( Color::Green );
 
-      app.draw(thr);
-      app.draw(five);
-      app.draw(sev);
+      one.setColor( Color::Red );
+      two.setColor( Color::Red );
+      four.setColor( Color::Red );
+      six.setColor( Color::Red );
+      eig.setColor( Color::Red );
+      nine.setColor( Color::Red );
+
+	update(app,one,two,thr,four,five,six,sev,eig,nine);
+	realWin = true;
     }
     /* FOR O */
 
@@ -134,32 +204,50 @@ void win(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,T
       two.setColor( Color::Green );
       thr.setColor( Color::Green );
 
-      app.draw(one);
-      app.draw(two);
-      app.draw(thr);
+      four.setColor( Color::Red );
+      five.setColor( Color::Red );
+      six.setColor( Color::Red );
+      sev.setColor( Color::Red );
+      eig.setColor( Color::Red );
+      nine.setColor( Color::Red );
+
+      update(app,one,two,thr,four,five,six,sev,eig,nine);
+      realWin = true;
     }
     if(c4 == 'O' && c5 == 'O' && c6 == 'O'){
     	cout << "Win O\n";
 
-      four.setColor( Color::Green );
-      five.setColor( Color::Green );
-      six.setColor( Color::Green );
+      	four.setColor( Color::Green );
+      	five.setColor( Color::Green );
+      	six.setColor( Color::Green );
 
-      app.draw(four);
-      app.draw(five);
-      app.draw(six);
+      	one.setColor( Color::Red );
+      	two.setColor( Color::Red );
+      	thr.setColor( Color::Red );
+      	sev.setColor( Color::Red );
+      	eig.setColor( Color::Red );
+      	nine.setColor( Color::Red );
+
+      	update(app,one,two,thr,four,five,six,sev,eig,nine);
+      	realWin = true;
     }
     if(c7 == 'O' && c8 == 'O' && c9 == 'O'){
-        cout << "Win O\n";
+      cout << "Win O\n";
 
-        sev.setColor( Color::Green );
-        eig.setColor( Color::Green );
-        nine.setColor( Color::Green );
+      	sev.setColor( Color::Green );
+      	eig.setColor( Color::Green );
+      	nine.setColor( Color::Green );
 
 
-        app.draw(sev);
-        app.draw(eig);
-        app.draw(nine);
+      	one.setColor( Color::Red );
+      	two.setColor( Color::Red );
+      	thr.setColor( Color::Red );
+      	four.setColor( Color::Red );
+      	five.setColor( Color::Red );
+      	six.setColor( Color::Red );
+
+      	update(app,one,two,thr,four,five,six,sev,eig,nine);
+      	realWin = true;
     }
     //O's Down
     if(c1 == 'O' && c4 == 'O' && c7 == 'O'){
@@ -169,9 +257,15 @@ void win(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,T
         four.setColor( Color::Green );
         sev.setColor( Color::Green );
 
-        app.draw(one);
-        app.draw(four);
-        app.draw(sev);
+        two.setColor( Color::Red );
+        thr.setColor( Color::Red );
+        five.setColor( Color::Red );
+        six.setColor( Color::Red );
+        eig.setColor( Color::Red );
+        nine.setColor( Color::Red );
+
+        update(app,one,two,thr,four,five,six,sev,eig,nine);
+        realWin = true;
     }
     if(c2 == 'O' && c5 == 'O' && c8 == 'O'){
         cout << "Win O\n";
@@ -180,9 +274,15 @@ void win(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,T
         five.setColor( Color::Green );
         eig.setColor( Color::Green );
 
-        app.draw(two);
-        app.draw(five);
-        app.draw(eig);
+        one.setColor( Color::Red );
+        thr.setColor( Color::Red );
+        four.setColor( Color::Red );
+        six.setColor( Color::Red );
+        sev.setColor( Color::Red );
+        nine.setColor( Color::Red );
+
+        update(app,one,two,thr,four,five,six,sev,eig,nine);
+        realWin = true;
     }
     if(c3 == 'O' && c6 == 'O' && c9 == 'O'){
         cout << "Win O\n";
@@ -191,9 +291,15 @@ void win(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,T
         six.setColor( Color::Green );
         nine.setColor( Color::Green );
 
-        app.draw(thr);
-        app.draw(six);
-        app.draw(nine);
+        one.setColor( Color::Red );
+        two.setColor( Color::Red );
+        four.setColor( Color::Red );
+        five.setColor( Color::Red );
+        sev.setColor( Color::Red );
+        eig.setColor( Color::Red );
+
+        update(app,one,two,thr,four,five,six,sev,eig,nine);
+        realWin = true;
     }
     //O's Cross
     if(c1 == 'O' && c5 == 'O' && c9 == 'O'){
@@ -203,9 +309,15 @@ void win(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,T
         five.setColor( Color::Green );
         nine.setColor( Color::Green );
 
-        app.draw(one);
-        app.draw(five);
-        app.draw(nine);
+        two.setColor( Color::Red );
+        thr.setColor( Color::Red );
+        four.setColor( Color::Red );
+        six.setColor( Color::Red );
+        sev.setColor( Color::Red );
+        eig.setColor( Color::Red );
+
+        update(app,one,two,thr,four,five,six,sev,eig,nine);
+        realWin = true;
     }if(c3 == 'O' && c5 == 'O' && c7 == 'O'){
         cout << "Win O\n";
 
@@ -213,9 +325,15 @@ void win(RenderWindow &app,Text &one,Text &two,Text &thr,Text &four,Text &five,T
         five.setColor( Color::Green );
         sev.setColor( Color::Green );
 
-        app.draw(thr);
-        app.draw(five);
-        app.draw(sev);
+        one.setColor( Color::Red );
+        two.setColor( Color::Red );
+        four.setColor( Color::Red );
+        six.setColor( Color::Red );
+        eig.setColor( Color::Red );
+        nine.setColor( Color::Red );
+
+        update(app,one,two,thr,four,five,six,sev,eig,nine);
+        realWin = true;
     }
 
 
@@ -289,6 +407,13 @@ int main(){
 	nine.setCharacterSize(50);
 	nine.setColor(Color::Black);
 	nine.move(110,110);
+
+	Sprite background;
+	Texture backgroundTexture;
+
+	backgroundTexture.loadFromFile("background.png");
+	background.setTexture(backgroundTexture);
+
 	while(window.isOpen()){
 		Event event;
 
@@ -301,8 +426,15 @@ int main(){
 				if(event.text.unicode < 128)
 					if (static_cast<char>(event.text.unicode) == '1' && bool1 == true)
 					{
-            c1 = ch;
+            			c1 = ch;
 						one.setString(ch);
+
+						if (realWin == true)
+						{
+							return 0;
+						}else{
+							
+						}
 
 						if (ch == 'X')
 						{
@@ -320,8 +452,15 @@ int main(){
 					}
 					if (static_cast<char>(event.text.unicode) == '2' && bool2 == true)
 					{
-            c2 = ch;
+            			c2 = ch;
 						two.setString(ch);
+
+						if (realWin == true)
+						{
+							return 0;
+						}else{
+							
+						}
 
 						if (ch == 'X')
 						{
@@ -339,10 +478,17 @@ int main(){
 					}
 					if (static_cast<char>(event.text.unicode) == '3' && bool3 == true)
 					{
-            c3 = ch;
+            			c3 = ch;
 						thr.setString(ch);
 
-            if (ch == 'X')
+						if (realWin == true)
+						{
+							return 0;
+						}else{
+							
+						}
+
+            			if (ch == 'X')
 						{
 							ch = 'O';
 						}else if (ch == 'O')
@@ -354,12 +500,19 @@ int main(){
 
 						window.draw(thr);
 						bool3 = false;
-            win(window,one,two,thr,four,five,six,sev,eig,nine);
+            			win(window,one,two,thr,four,five,six,sev,eig,nine);
 					}
 					if (static_cast<char>(event.text.unicode) == '4' && bool4 == true)
 					{
-            c4 = ch;
+            			c4 = ch;
 						four.setString(ch);
+
+						if (realWin == true)
+						{
+							return 0;
+						}else{
+							
+						}
 
 						if (ch == 'X')
 						{
@@ -373,12 +526,19 @@ int main(){
 
 						window.draw(four);
 						bool4 = false;
-            win(window,one,two,thr,four,five,six,sev,eig,nine);
+            			win(window,one,two,thr,four,five,six,sev,eig,nine);
 					}
 					if (static_cast<char>(event.text.unicode) == '5' && bool5 == true)
 					{
-            c5 = ch;
+            			c5 = ch;
 						five.setString(ch);
+
+						if (realWin == true)
+						{
+							return 0;
+						}else{
+							
+						}
 
 						if (ch == 'X')
 						{
@@ -392,12 +552,19 @@ int main(){
 
 						window.draw(five);
 						bool5 = false;
-            win(window,one,two,thr,four,five,six,sev,eig,nine);
+            			win(window,one,two,thr,four,five,six,sev,eig,nine);
 					}
 					if (static_cast<char>(event.text.unicode) == '6' && bool6 == true)
 					{
-            c6 = ch;
+            			c6 = ch;
 						six.setString(ch);
+
+						if (realWin == true)
+						{
+							return 0;
+						}else{
+							
+						}
 
 						if (ch == 'X')
 						{
@@ -411,12 +578,20 @@ int main(){
 
 						window.draw(six);
 						bool6 = false;
-            win(window,one,two,thr,four,five,six,sev,eig,nine);
+            			win(window,one,two,thr,four,five,six,sev,eig,nine);
 					}
 					if (static_cast<char>(event.text.unicode) == '7' && bool7 == true)
 					{
-            c7 = ch;
+            			c7 = ch;
 						sev.setString(ch);
+
+						if (realWin == true)
+						{
+							return 0;
+						}else{
+							
+						}
+
 						if (ch == 'X')
 						{
 							ch = 'O';
@@ -428,14 +603,21 @@ int main(){
 						}
 
 						window.draw(sev);
-            bool7 = false;
-            win(window,one,two,thr,four,five,six,sev,eig,nine);
+            			bool7 = false;
+            			win(window,one,two,thr,four,five,six,sev,eig,nine);
 
 					}
 					if (static_cast<char>(event.text.unicode) == '8' && bool8 == true)
 					{
-            c8 = ch;
+            			c8 = ch;
 						eig.setString(ch);
+
+						if (realWin == true)
+						{
+							return 0;
+						}else{
+							
+						}
 
 						if (ch == 'X')
 						{
@@ -449,12 +631,19 @@ int main(){
 
 						window.draw(eig);
 						bool8 = false;
-            win(window,one,two,thr,four,five,six,sev,eig,nine);
-          }
+            			win(window,one,two,thr,four,five,six,sev,eig,nine);
+          			}
 					if (static_cast<char>(event.text.unicode) == '9' && bool9 == true)
 					{
 						c9 = ch;
 						nine.setString(ch);
+
+						if (realWin == true)
+						{
+							return 0;
+						}else{
+						
+						}
 
 						if (ch == 'X')
 						{
@@ -468,11 +657,49 @@ int main(){
 
 						window.draw(nine);
 						bool9 = false;
-            win(window,one,two,thr,four,five,six,sev,eig,nine);
+            			win(window,one,two,thr,four,five,six,sev,eig,nine);
+					}
+					if (static_cast<char>(event.text.unicode) == 'q' || static_cast<char>(event.text.unicode) == 'Q')
+					{
+						ch = 'X'; // Reset x
+						c1 = '1';
+						c2 = '2';
+						c3 = '3';
+						c4 = '4';
+						c5 = '5';
+						c6 = '6';
+						c7 = '7';
+						c8 = '8';
+						c9 = '9';
+						/// Booleans ///
+						bool1 = true;
+						bool2 = true;
+						bool3 = true;
+						bool4 = true;
+						bool5 = true;
+						bool6 = true;
+						bool7 = true;
+						bool8 = true;
+						bool9 = true;
+
+						one.setString("1");
+						two.setString("2");
+						thr.setString("3");
+						four.setString("4");
+						five.setString("5");
+						six.setString("6");
+						sev.setString("7");
+						eig.setString("8");
+						nine.setString('9');
+						resetColor(window,one,two,thr,four,five,six,sev,eig,nine);
+						update(window,one,two,thr,four,five,six,sev,eig,nine);
+
+						realWin = false;
 					}
 
 		}
 		window.clear(Color::White);
+		window.draw(background);
 		window.draw(one);
 		window.draw(two);
 		window.draw(thr);
